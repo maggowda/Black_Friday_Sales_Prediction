e57be62b36a408d315d0893410cf3ab0fd9335fe
MEGHARSHA A GOWDA <meghaharsha72@gmail.com>
2025-01-08 23:46:43 +0530

    streamlit

---
+++ app.py
@@ -0,0 +1,31 @@
+# Core Pkgs
+import streamlit as st 
+import streamlit.components.v1 as stc 
+from eda_app import run_eda
+from ml_app import run_ml
+
+html_temp = """
+		<div style="background-color:#3872fb;padding:10px;border-radius:10px">
+		<h1 style="color:white;text-align:center;">Black Friday Sales App</h1>
+		<h4 style="color:white;text-align:center;">Happy Thanksgiving</h4>
+		</div>
+		"""
+
+def main():
+	stc.html(html_temp)
+	menu = ["Home","EDA","ML","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
+
+	
+	if choice == "EDA":
+		run_eda()
+	elif choice == "ML":
+		run_ml()
+	else:
+		st.subheader("About")
+		st.info("Built with Streamlit")
+
+
+
+if __name__ == '__main__':
+	main()
